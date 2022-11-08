cec628845feecc2855e8ad09d31a66c504f41565
LearnToUnderstand <29694600+LearnToUnderstand@users.noreply.github.com>
2022-11-07 19:59:33 -0500

    minor changes

--- neton.sh
+++ neton.sh
@@ -24,26 +24,26 @@ Which website would you like to start out on?
 	if [ $ANS_2 == 1 ]; then
 		printf "\n\n  Duck Duck Go, baby!\n\n"
 		espeak -s40 -p20 "duck dukck go. baby."
-		exec brave-browser --incognito duckduckgo.com 
+		exec brave-browser --incognito duckduckgo.com&
 	elif [ $ANS_2 == 2 ]; then
 		echo "
 YouTube, baby! 
 		"
 		espeak -s40 -p20 "you tube. baby. "
-		exec brave-browser --incognito youtube.com
+		exec brave-browser --incognito youtube.com&
 	elif [ $ANS_2 == 3 ]; then
 		echo "
 Watch out for the distraction pit...
 		       "	
 		espeak -s40 -p20 "watch out for the distraction pit..."
-		exec brave-browser --incognitor reddit.com
+		exec brave-browser --incognitor reddit.com&
 	elif [ $ANS_2 == 4 ]; then
 		echo "
 Don't spend too much... 
 		"
 		espeak -s40 -p20 "don't. spend. too. much."
-		exec brave-browser --incognito amazon.com
+		exec brave-browser --incognito amazon.com&
 	else
-		exec brave-browser --incognito
+		exec brave-browser --incognito&
 	fi
 fi
